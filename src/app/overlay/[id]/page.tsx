9a3871440277e4430c0a7828e465271754b87144
Quentin Verdier <quent1.verdier@gmail.com>
2025-08-31 17:55:00 +0200

    ✨ add base overlay page

---
+++ src/app/overlay/[id]/page.tsx
@@ -0,0 +1,8 @@
+export default function Overlay() {
+  return (
+    <>
+      <div className="bg-opacity-10 absolute bottom-0 left-56 flex size-56 items-center justify-center bg-black backdrop-blur-sm"></div>
+      <div className="bg-opacity-10 absolute right-56 bottom-0 flex size-56 items-center justify-center bg-black backdrop-blur-sm"></div>
+    </>
+  );
+}
