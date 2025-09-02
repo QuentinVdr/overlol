509d4d79edec1d97619a50cf1fa0b0b996c9f791
Quentin VERDIER <quent1.verdier@gmail.com>
2025-09-02 18:45:01 +0200

    🎨 Rearrange PlayerForm layout for improved structure and readability

--- src/components/OverlayForm/PlayersForm/PlayerForm.tsx
+++ src/components/OverlayForm/PlayersForm/PlayerForm.tsx
@@ -17,25 +17,14 @@ export function PlayerForm({ register, setValue, fieldPrefix }: Readonly<PlayerF
 
   return (
     <div className="flex flex-row gap-2">
-      <div className="flex grow flex-col">
-        <label htmlFor={`champion-${fieldPrefix}`} className="pb-1 pl-0.5 text-xs text-gray-500">
-          Champion
-        </label>
-        <input
-          id={`champion-${fieldPrefix}`}
-          type="text"
-          className="grow rounded-lg border border-gray-300 bg-white px-2 py-1 shadow-sm transition-all outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-500"
-          {...register(`${fieldPrefix}.championName`)}
-        />
-      </div>
-      <div className="flex flex-col">
+      <div className="flex w-20 flex-col">
         <label htmlFor={`team-${fieldPrefix}`} className="pb-1 pl-0.5 text-xs text-gray-500">
           Équipe
         </label>
         <input
           id={`team-${fieldPrefix}`}
           type="text"
-          className="w-32 rounded-lg border border-gray-300 bg-white px-2 py-1 shadow-sm transition-all outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-500"
+          className="rounded-lg border border-gray-300 bg-white px-2 py-1 shadow-sm transition-all outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-500"
           {...register(`${fieldPrefix}.teamName`)}
         />
       </div>
@@ -48,6 +37,17 @@ export function PlayerForm({ register, setValue, fieldPrefix }: Readonly<PlayerF
           type="text"
           className="grow rounded-lg border border-gray-300 bg-white px-2 py-1 shadow-sm transition-all outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-500"
           {...register(`${fieldPrefix}.playerName`)}
+        />
+      </div>
+      <div className="flex grow flex-col">
+        <label htmlFor={`champion-${fieldPrefix}`} className="pb-1 pl-0.5 text-xs text-gray-500">
+          Champion
+        </label>
+        <input
+          id={`champion-${fieldPrefix}`}
+          type="text"
+          className="grow rounded-lg border border-gray-300 bg-white px-2 py-1 shadow-sm transition-all outline-none focus:border-blue-500 focus:ring-2 focus:ring-blue-500"
+          {...register(`${fieldPrefix}.championName`)}
         />
       </div>
       <div className="flex flex-col justify-end">
