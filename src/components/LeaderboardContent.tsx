7cdde08245462910f16742f9db39e39a7610001d
Quentin VERDIER <quent1.verdier@gmail.com>
2025-09-10 16:31:08 +0200

    🎨 Update styles in LeaderboardContent for improved layout and readability

--- src/components/LeaderboardContent.tsx
+++ src/components/LeaderboardContent.tsx
@@ -6,7 +6,7 @@ export default function LeaderboardContent() {
   const { data: leaderboard } = useFetchKcLeaderboard();
 
   return (
-    <div className="absolute top-[146px] left-[69px] flex flex-col gap-2 text-xl font-bold text-white">
+    <div className="absolute top-[148px] left-[72px] flex h-174 w-[454px] flex-col justify-around gap-2 bg-zinc-900/40 text-[21px] font-bold text-white">
       {leaderboard?.map((player) => (
         <div key={player.kcLeaderboardPosition + player.player}>
           #{player.kcLeaderboardPosition}. {player.player}:{' '}
