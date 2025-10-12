a07a18ea4f53248709f92d704eae7aa63410a9f4
Quentin VERDIER <quent1.verdier@gmail.com>
2025-10-12 19:58:10 +0200

    ✨ Add cacheMaxMemorySize configuration to next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,7 @@ import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
   output: 'standalone',
+  cacheMaxMemorySize: 50 * 1024 * 1024, // 50 MB
 };
 
 export default nextConfig;
