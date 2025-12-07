020689afa9c8a9858bcb76937a6c13216f11d7a7
Quentin VERDIER <quent1.verdier@gmail.com>
2025-12-07 20:13:27 +0100

    🎨 Update kcPlayerList to include Kyeahoo and Busio accounts

--- src/lib/leaderboardApi.ts
+++ src/lib/leaderboardApi.ts
@@ -10,11 +10,17 @@ const kcPlayerList: { [key: string]: TRiotAccount[] } = {
     { riotPseudo: 'K C', tagLine: 'kcwin', region: 'EUW' },
   ],
   Yike: [{ riotPseudo: 'KC Yiken', tagLine: '1111', region: 'EUW' }],
+  Kyeahoo: [{ riotPseudo: 'superkimchi', tagLine: '123', region: 'EUW' }],
   Caliste: [
     { riotPseudo: 'KC NEXT ADKING', tagLine: 'EUW', region: 'EUW' },
     { riotPseudo: 'I NEED SOLOQ', tagLine: 'EUW', region: 'EUW' },
   ],
+  Busio: [
+    { riotPseudo: 'Busio JNG', tagLine: 'NA1', region: 'EUW' },
+    { riotPseudo: 'OG Mithy', tagLine: 'Moo', region: 'EUW' },
+  ],
   // KC Blue Roster
+  Tao: [{ riotPseudo: 'xhtao', tagLine: '3100', region: 'EUW' }],
   Yukino: [
     { riotPseudo: 'yukino cat', tagLine: 'blue', region: 'EUW' },
     { riotPseudo: 'yukino cat', tagLine: 'cat', region: 'EUW' },
@@ -24,20 +30,19 @@ const kcPlayerList: { [key: string]: TRiotAccount[] } = {
     { riotPseudo: 'TODOROKI RAICHI', tagLine: 'rank1', region: 'EUW' },
     { riotPseudo: 'Labubu IRL', tagLine: 'macha', region: 'EUW' },
   ],
-  Prime: [
-    { riotPseudo: 'Céleste', tagLine: '6162', region: 'EUW' },
-    { riotPseudo: 'POBC', tagLine: '6162', region: 'EUW' },
-  ],
-  // KC BS Roster
-  Tao: [{ riotPseudo: 'xhtao', tagLine: '3100', region: 'EUW' }],
-  BAASHH: [{ riotPseudo: 'TTV BAASHH', tagLine: 'EUW', region: 'EUW' }],
-  MathisV: [{ riotPseudo: 'MathisV', tagLine: 'ARCHE', region: 'EUW' }],
   Hazel: [
     { riotPseudo: 'Antarctica', tagLine: 'S B', region: 'EUW' },
     { riotPseudo: 'Hazel', tagLine: 'KCorp', region: 'EUW' },
     { riotPseudo: 'one last dance', tagLine: '114', region: 'EUW' },
     { riotPseudo: '114', tagLine: '1405', region: 'EUW' },
   ],
+  Prime: [
+    { riotPseudo: 'Céleste', tagLine: '6162', region: 'EUW' },
+    { riotPseudo: 'POBC', tagLine: '6162', region: 'EUW' },
+  ],
+  // KC BS Roster
+  BAASHH: [{ riotPseudo: 'TTV BAASHH', tagLine: 'EUW', region: 'EUW' }],
+  MathisV: [{ riotPseudo: 'MathisV', tagLine: 'ARCHE', region: 'EUW' }],
   Nsurr: [
     { riotPseudo: 'TripleMonstre', tagLine: 'EUWFR', region: 'EUW' },
     { riotPseudo: 'Full Drip Nsurr', tagLine: 'EUW', region: 'EUW' },
